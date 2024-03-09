200442b47cc4dc1b8e7127be49feb2e10638b449
Austin Bieber <ajbieber1995@gmail.com>
2024-03-09 10:32:48 -0700

    Properly handling session before login

--- pages/index.js
+++ pages/index.js
@@ -14,22 +14,33 @@ import { jwtDecode } from 'jwt-decode';
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home(props) {
-  const { user } = useUser();  
+  const { user, isLoading, error } = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+
   return (
     <Layout>
       <Navigation user={user} />
       {user && (
         <strong>Please ask an admin to add you as user!</strong>
       )}
-      < LandingPage user={user}></ LandingPage>
+      <LandingPage user={user}></LandingPage>
     </Layout>
   );
 }
 
 
 export async function getServerSideProps(ctx) {
-  const { user, idToken } = await getSession(ctx.req, ctx.res);
+  const session = await getSession(ctx.req, ctx.res);
+
+  // User hasn't logged in yet
+  if (!session) {
+    return { props: {} }
+  }
+
+  const { user, idToken } = session;
   const idTokenDecoded = jwtDecode(idToken);
   user.isAdmin = idTokenDecoded['https://visionaid.org/roles'].includes('Admin');
-  return user;
+  return { props: user };
 }
