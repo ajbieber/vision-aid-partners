c0ee87e6f14ed5af0eb528c907c5525767d72aa7
Christin Lin <christinlin17@gmail.com>
2024-03-31 15:08:57 -0700

    Update landingpage.js

--- pages/landingpage.js
+++ pages/landingpage.js
@@ -1,140 +1,62 @@
-import React  from 'react';
-
-import Image from 'next/image'
+import React from 'react';
+import Image from 'next/image';
 import p1 from 'public/images/p1.webp';
 import p2 from 'public/images/p2.webp';
-import p3 from 'public/images/p3.webp';  
+import p3 from 'public/images/p3.webp';
 
-function  LandingPage(props) {
+function Post({ title, content, date }) {
     return (
-        <div>
-            <div className={"scroller"}>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <div>
-                <b>Post Title 1</b>
-                <br></br>
-                <span>Post Content 1</span>
-                <br></br>
-                <span>Date: March 23, 2024</span>
-                <br></br>
-                <br></br>
-                <br></br>
-                <b>Post Title 2</b>
-                <br></br>
-                <span>Post Content 2</span>
-                <br></br>
-                <span>Date: March 23, 2024</span>
-                <br></br>
-                <br></br>
-                <br></br>
-                <br></br>
-                More stories of success..
-                <br></br>
-                <br></br>
-                <br></br>
-                <br></br>
-                Thanks!
-                <br></br>
-                <br></br>
-                <br></br>
-                <br></br>
-                <br></br>
-                <br></br>
-            </div>
-                <b> Vision-Aid and OMSCS collaboration</b>
-                <br></br>
-                <br></br>
-                <br></br>
-                <br></br>
-                <br></br>
-                <br></br>
-                <br></br>
-                <div>
-                    {/* <div className={"box box1"}> <img src="https://visionaid.org/wp-content/uploads/2024/01/DSC_9437-1536x1028.jpg" alt="Vision-Aid’s Journey of Empowerment at Sankara Nethralaya, Chennai" /> </div> */}
-                    <div className="img-grid">
-                        <Image
-                            src={p1}
-                            width={200}
-                            height={100}
-                            alt=""
-                            />
-                    </div>                
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    {/* <div className={"box box1"}> <img src="https://visionaid.org/wp-content/uploads/2024/02/image.jpeg" alt="About the center in SN Chennai" /> </div>   */}
-                    <div className="img-grid">
-                        <Image
-                            // src="https://visionaid.org/wp-content/uploads/2024/02/image.jpeg"
-                            src={p2}
-                            width={300}
-                            height={200}
-                            alt=""
-                            />
-                    </div>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    {/* <div className={"box box1"}> <img src="https://visionaid.org/wp-content/uploads/2024/01/image_2024-01-31_192032823.png" /> </div>   */}
-                    <div className="img-grid">
-                        <Image
-                            src={p3}
-                            width={200}
-                            height={200}
-                            alt=""
-                        />
-                    </div>
-                    
-                    
-                    
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    More stories of success..
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    Thanks!
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                </div>      
-            </div>
+        <div style={{ marginBottom: '20px' }}>
+            <h4>{title}</h4>
+            <p>{content}</p>
+            <p>Date: {date}</p>
         </div>
     );
 }
 
+function LandingPage(props) {
+    return (
+        <div>
+            <h3>Posts</h3>
+            <div className="scroller">
+                {[...Array(2)].map((_, index) => (
+                    <Post
+                        key={index}
+                        title={`Post Title ${index + 1}`}
+                        content={`Post Content ${index + 1}`}
+                        date="March 23, 2024"
+                    />
+                ))}
+                <h3>Vision-Aid and OMSCS collaboration</h3>
+                <div style={{ marginBottom: '20px' }}>
+                    <Image
+                        src={p1}
+                        width={200}
+                        height={100}
+                        alt=""
+                    />
+                </div>
+                <div style={{ marginBottom: '20px' }}>
+                    <Image
+                        src={p2}
+                        width={300}
+                        height={200}
+                        alt=""
+                    />
+                </div>
+                <div style={{ marginBottom: '20px' }}>
+                    <Image
+                        src={p3}
+                        width={200}
+                        height={200}
+                        alt=""
+                    />
+                </div>
+                <p>More stories of success..</p>
+                <p>Thanks!</p>
+            </div>
+        </div>
+    );
+}
 
 export default LandingPage;
