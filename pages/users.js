c681a3f6973d37bbd48c7c24cd361213ad0033d9
Austin Bieber <ajbieber1995@gmail.com>
2024-04-10 10:19:45 -0600

    Fixed issue with user create not calling API

--- pages/users.js
+++ pages/users.js
@@ -73,7 +73,7 @@ function NewUserModal(props) {
   }
 
   const handleSubmit = async () => {
-    const response = await fetch("", {
+    const response = await fetch("/api/user", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
